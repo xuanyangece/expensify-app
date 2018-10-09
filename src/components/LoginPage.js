bb151b4b3eb08fb40e1cf5550746db0059c26240
Xuan Yang <xuanyangee@yahoo.com>
2018-10-08 23:38:34 -0400

    refactor landing page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify app</h1>
+      <p>Get every cost under control.</p>
+      <p>(Inspired by Andrew Mead)</p>
+      <button className="button" onClick={startLogin}>Login with Google</button>
+    </div>
   </div>
 );
 
