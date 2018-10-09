ed94747da74008d8aed0686f145401eb6d679eb0
Xuan Yang <xuanyangee@yahoo.com>
2018-10-09 00:17:22 -0400

    Add styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,8 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Expensify app</h1>
-      <p>Get every cost under control.</p>
-      <p>(Inspired by Andrew Mead)</p>
+      <h1 className="box-layout__title">Expensify</h1>
+      <p>It's time to get your expenses under control.</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
